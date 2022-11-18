f13e245ddf8a39aad699bdd318ae480988009849
LuisPontesLS <leospontes@hotmail.com>
2022-11-18 17:11:07 -0300

    Bandeiras, todos os países da copa 2022

--- main.js
+++ main.js
@@ -34,6 +34,22 @@ function creatCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
+  creatCard("20/11", "domingo", createGame("qatar", "13:00", "ecuador")) +
+  creatCard(
+    "21/11",
+    "segunda",
+    createGame("england", "10:00", "iran") +
+      createGame("senegal", "13:00", "netherlands") +
+      createGame("USA", "16:00", "wales")
+  ) +
+  creatCard(
+    "22/11",
+    "terça",
+    createGame("argentina", "07:00", "saudi-arabia") +
+      createGame("denmark", "10:00", "tunisia") +
+      createGame("mexico", "13:00", "poland") +
+      createGame("france", "16:00", "australia")
+  ) +
   creatCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
   creatCard(
     "28/11",
